929b0db9c2a35f09b466337ae8165799f7af8188
Touko Haapanen <toukoh@Touko-MacBook-Pro.local>
2023-11-28 14:10:12 +0200

    labeled data

---
+++ scripts/label_columns.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data_file_path = "data/chord_data.csv"
+
+df = pd.read_csv(data_file_path, header=None)
+
+column_names = [
+    "Chord Root Note", "Chord Type",
+    "Chroma A", "Chroma A#", "Chroma B", "Chroma C", "Chroma C#", "Chroma D", 
+    "Chroma D#", "Chroma E", "Chroma F", "Chroma F#", "Chroma G", "Chroma G#"
+]
+
+df.columns = column_names
+
+output_file = "data/labeled_music_data.csv"
+df.to_csv(output_file, index=False)
+
+output_file
